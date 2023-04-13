2a8ae9acc408845151503640068b2ed08857f606
udaoewka <ksm54239@gmail.com>
2023-04-13 16:00:37 +0900

    2개 공부한거 커밋

--- src/main/java/com/ksm/exam/demo/controller/UserHomeController.java
+++ src/main/java/com/ksm/exam/demo/controller/UserHomeController.java
@@ -7,10 +7,55 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class UserHomeController {
 	
+	
 	@RequestMapping("/usr/home/main")
 	@ResponseBody
 	public String showMain() {
-		return "안녕하세여.";
+		return "안녕하세요.";
 	}
-
+	
+	@RequestMapping("/usr/home/main2")
+	@ResponseBody
+	public String showMain2() {
+		return "반갑습니다.";
+	}
+	
+	@RequestMapping("/usr/home/main3")
+	@ResponseBody
+	public String showMain3() {
+		return "또 만나요.";
+	}
+	
+	private int count;
+	
+	public UserHomeController(){
+		count = -1;
+	}
+	
+	@RequestMapping("/usr/home/main4")
+	@ResponseBody
+	public int showMain4() {
+		count++;
+		return count;
+	}
+	
+	@RequestMapping("/usr/home/main5")
+	@ResponseBody
+	public String showMain5() {
+		return "count를 초기화 되었습니다.";
+	}
+	
+	@RequestMapping("/usr/home/getCount")
+	@ResponseBody
+	public int getCount() {
+		return count;
+	}
+	
+	@RequestMapping("/usr/home/doSetCount")
+	@ResponseBody
+	public String doSetCount(int count) {
+		this.count = count;
+		return "count의 값이 " + this.count + "으로 초기화.";
+	}
+	
 }
