784dc751ace71900caae1b173431e1cb944a19d3
udaoewka <ksm54239@gmail.com>
2023-04-18 15:17:50 +0900

    2시간 동안 한 수업 올림

--- src/main/java/com/ksm/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/ksm/exam/demo/controller/UsrArticleController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ksm.exam.demo.service.ArticleService;
+import com.ksm.exam.demo.utill.Ut;
 import com.ksm.exam.demo.vo.Article;
+import com.ksm.exam.demo.vo.ResultData;
 
 @Controller
 public class UsrArticleController {
@@ -18,30 +20,42 @@ public class UsrArticleController {
 	// 액션 메서드 시작
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
-	public Article doAdd(String title, String body) {
-		int id = articleService.writeArticle(title, body);
+	public ResultData doAdd(String title, String body) {
+		if(Ut.empty(title)) {
+			return ResultData.from("F-1", "title(을)를 입력해주세요.");
+		}
+		
+		if(Ut.empty(body)) {
+			return ResultData.from("F-2", "body(을)를 입력해주세요.");
+		}
+		
+		ResultData writeArticleRd = articleService.writeArticle(title, body);
+		
+		int id = (int)writeArticleRd.getData1();
 		
 		Article article = articleService.getArticle(id);
 		
-		return article;
+		return ResultData.from(writeArticleRd.getResultCode(), writeArticleRd.getMsg(), article);
 	}
 	
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
-	public List<Article> getArticles() {
-		return articleService.getArticles();
+	public ResultData getArticles() {
+		List<Article> articles = articleService.getArticles();
+		
+		return ResultData.from("S-1", "게시물 리스트입니다.", articles);
 	}
 	
 	@RequestMapping("/usr/article/getArticle")
 	@ResponseBody
-	public Object getArticle(int id) {
+	public ResultData getArticle(int id) {
 		Article article = articleService.getArticle(id);
 		
 		if ( article == null ) {
-			return id + "번 게시물이 존재하지 않습니다.";
+			return ResultData.from("F-1",Ut.f("%d번 게시물이 존재하지 않습니다.", id));
 		}
 		
-		return article;
+		return ResultData.from("S-1", Ut.f("%d번 게시물입니다.", id), article);
 	}
 	
 	@RequestMapping("/usr/article/doDelete")
