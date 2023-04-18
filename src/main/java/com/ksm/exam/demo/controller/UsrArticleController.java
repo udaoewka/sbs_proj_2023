881e055ec8146acb08219d787c8edfd9977e8711
udaoewka <ksm54239@gmail.com>
2023-04-18 16:14:02 +0900

    2시간 수업한것 올림

--- src/main/java/com/ksm/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/ksm/exam/demo/controller/UsrArticleController.java
@@ -21,17 +21,17 @@ public class UsrArticleController {
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public ResultData doAdd(String title, String body) {
-		if(Ut.empty(title)) {
+		if ( Ut.empty(title) ) {
 			return ResultData.from("F-1", "title(을)를 입력해주세요.");
 		}
 		
-		if(Ut.empty(body)) {
+		if ( Ut.empty(body) ) {
 			return ResultData.from("F-2", "body(을)를 입력해주세요.");
 		}
 		
-		ResultData writeArticleRd = articleService.writeArticle(title, body);
+		ResultData<Integer> writeArticleRd = articleService.writeArticle(title, body);
 		
-		int id = (int)writeArticleRd.getData1();
+		int id = writeArticleRd.getData1();
 		
 		Article article = articleService.getArticle(id);
 		
@@ -52,7 +52,7 @@ public class UsrArticleController {
 		Article article = articleService.getArticle(id);
 		
 		if ( article == null ) {
-			return ResultData.from("F-1",Ut.f("%d번 게시물이 존재하지 않습니다.", id));
+			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
 		}
 		
 		return ResultData.from("S-1", Ut.f("%d번 게시물입니다.", id), article);
@@ -60,30 +60,30 @@ public class UsrArticleController {
 	
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
-	public String doDelete(int id) {
+	public ResultData<Integer> doDelete(int id) {
 		Article article = articleService.getArticle(id);
 		
 		if ( article == null ) {
-			return id + "번 게시물이 존재하지 않습니다.";
+			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
 		}
 		
 		articleService.deleteArticle(id);
 		
-		return id + "번 게시물을 삭제하였습니다.";
+		return ResultData.from("S-1", Ut.f("%d번 게시물을 삭제하였습니다.", id), id);
 	}
 	
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
-	public String doModify(int id, String title, String body) {
+	public ResultData<Integer> doModify(int id, String title, String body) {
 		Article article = articleService.getArticle(id);
 		
 		if ( article == null ) {
-			return id + "번 게시물이 존재하지 않습니다.";
+			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
 		}
 		
 		articleService.modifyArticle(id, title, body);
 		
-		return id + "번 게시물을 수정하였습니다.";
+		return ResultData.from("S-1", Ut.f("%d번 게시물을 수정하였습니다.", id), id);
 	}
 	// 액션 메서드 끝
 
--- src/main/java/com/ksm/exam/demo/service/ArticleService.java
+++ src/main/java/com/ksm/exam/demo/service/ArticleService.java
@@ -25,9 +25,10 @@ public class ArticleService {
 		return articleRepository.getArticles();
 	}
 	
-	public ResultData writeArticle(String title, String body) {
+	public ResultData<Integer> writeArticle(String title, String body) {
 		articleRepository.writeArticle(title, body);
 		int id = articleRepository.getLastInsertId();
+		
 		return ResultData.from("S-1", Ut.f("%d번 게시물이 생성되었습니다.", id), id);
 	}
 	
