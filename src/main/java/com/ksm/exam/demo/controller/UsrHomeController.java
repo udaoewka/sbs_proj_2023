14a28e7e7eb5102763125163f0a942c43809550a
udaoewka <ksm54239@gmail.com>
2023-04-19 15:14:22 +0900

    JSP 연결

--- src/main/java/com/ksm/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/ksm/exam/demo/controller/UsrHomeController.java
@@ -5,13 +5,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class UserHomeController {
+public class UsrHomeController {
 	
 	
 	@RequestMapping("/usr/home/main")
-	@ResponseBody
 	public String showMain() {
-		return "안녕하세요.";
+		return "/usr/home/main";
+	}
+	
+	@RequestMapping("/")
+	public String showRoot() {
+		return "redirect:/usr/home/main";
 	}
 	
 	@RequestMapping("/usr/home/main2")
@@ -28,7 +32,7 @@ public class UserHomeController {
 	
 	private int count;
 	
-	public UserHomeController(){
+	public UsrHomeController(){
 		count = -1;
 	}
 	
