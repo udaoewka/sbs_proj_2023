a545b57daab60f0cbd0e4f0d79b161fdce43d61d
udaoewka <ksm54239@gmail.com>
2023-04-20 17:47:53 +0900

    Rq객체를 BeforAction인터셉터에서 대표로 한번만 사용이 가능하도록 구현

---
+++ src/main/java/com/ksm/exam/demo/MyWebMvcConfigurer.java
@@ -0,0 +1,21 @@
+package com.ksm.exam.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.ksm.exam.demo.interceptor.BeforeActionInterceptor;
+
+@Configuration
+public class MyWebMvcConfigurer implements WebMvcConfigurer {
+	// BeforeActionInterceptor 불러오기
+	@Autowired
+	BeforeActionInterceptor beforeActionInterceptor;
+	
+	// 이 함수는 인터셉터를 적용하는 역할을 합니다.
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/error");
+	}
+}
---
+++ src/main/java/com/ksm/exam/demo/interceptor/BeforeActionInterceptor.java
@@ -0,0 +1,20 @@
+package com.ksm.exam.demo.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.ksm.exam.demo.vo.Rq;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class BeforeActionInterceptor implements HandlerInterceptor {
+	@Override
+	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
+		Rq rq = new Rq(req);
+		req.setAttribute("rq", rq);
+
+		return HandlerInterceptor.super.preHandle(req, resp, handler);
+	}
+}
