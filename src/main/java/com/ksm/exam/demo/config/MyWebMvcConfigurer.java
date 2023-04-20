99b12964633366f364ece08e12b4575f15c746b4
udaoewka <ksm54239@gmail.com>
2023-04-20 18:35:40 +0900

    NeedLogin인터셉터가 여러 액션들을 대신해서 자격없는 ㅇ청들은 앞에서 거름

--- src/main/java/com/ksm/exam/demo/config/MyWebMvcConfigurer.java
+++ src/main/java/com/ksm/exam/demo/config/MyWebMvcConfigurer.java
@@ -1,4 +1,4 @@
-package com.ksm.exam.demo;
+package com.ksm.exam.demo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.ksm.exam.demo.interceptor.BeforeActionInterceptor;
+import com.ksm.exam.demo.interceptor.NeedLoginInterceptor;
 
 @Configuration
 public class MyWebMvcConfigurer implements WebMvcConfigurer {
@@ -13,9 +14,15 @@ public class MyWebMvcConfigurer implements WebMvcConfigurer {
 	@Autowired
 	BeforeActionInterceptor beforeActionInterceptor;
 	
+	// NeedLoginInterceptor 불러오기
+	@Autowired
+	NeedLoginInterceptor needLoginInterceptor;
+	
 	// 이 함수는 인터셉터를 적용하는 역할을 합니다.
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/error");
+		registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/error").excludePathPatterns("/resource/**");
+		
+		registry.addInterceptor(needLoginInterceptor).addPathPatterns("/usr/article/write").addPathPatterns("/usr/article/doWrite").addPathPatterns("/usr/article/modify").addPathPatterns("/usr/article/doModify").addPathPatterns("/usr/article/doDelete");
 	}
 }
--- src/main/java/com/ksm/exam/demo/vo/Rq.java
+++ src/main/java/com/ksm/exam/demo/vo/Rq.java
@@ -1,6 +1,11 @@
 package com.ksm.exam.demo.vo;
 
+import java.io.IOException;
+
+import com.ksm.exam.demo.utill.Ut;
+
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.Getter;
 
@@ -10,7 +15,13 @@ public class Rq {
 	@Getter
 	private int loginedMemberId;
 	
-	public Rq(HttpServletRequest req) {
+	private HttpServletRequest req;
+	private HttpServletResponse resp;
+	
+	public Rq(HttpServletRequest req, HttpServletResponse resp) {
+		this.req = req;
+		this.resp = resp;
+		
 		HttpSession httpSession = req.getSession();
 		
 		boolean isLogined = false;
@@ -23,6 +34,31 @@ public class Rq {
 		
 		this.isLogined = isLogined;
 		this.loginedMemberId = loginedMemberId;
+	}
+
+	public void printHistoryBackjs(String msg) {
+		resp.setContentType("text/html; charset=UTF-8");
+		println("<script>");
+		
+		if(!Ut.empty(msg)) {
+			println("alert('"+ msg + "');");
+		}
+		
+		println("history.back();");
+		
+		println("</script>");
+	}
+	
+	public void print(String str) {
+		try {
+			resp.getWriter().append(str);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void println(String str) {
+		print(str + "\n");
 	}
 
 }
