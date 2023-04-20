0468293a9422477746f400fe77451c0b5de22c12
udaoewka <ksm54239@gmail.com>
2023-04-20 16:04:28 +0900

    삭제버튼은 자기가 쓴것만 가능하게 수정

--- src/main/java/com/ksm/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/ksm/exam/demo/controller/UsrArticleController.java
@@ -49,14 +49,22 @@ public class UsrArticleController {
 		
 		int id = writeArticleRd.getData1();
 		
-		Article article = articleService.getForPrintArticle(id);
+		Article article = articleService.getForPrintArticle(loginedMemberId, id);
 		
 		return ResultData.from(writeArticleRd.getResultCode(), writeArticleRd.getMsg(), "article", article);
 	}
 	
 	@RequestMapping("/usr/article/list")
-	public String showList(Model model) {
-		List<Article> articles = articleService.getForPrintArticles();
+	public String showList(HttpSession httpSession, Model model) {
+		boolean isLogined = false;
+		int loginedMemberId = 0;
+		
+		if ( httpSession.getAttribute("loginedMemberId") != null) {
+			isLogined = true;
+			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
+		}
+		
+		List<Article> articles = articleService.getForPrintArticles(loginedMemberId);
 		
 		model.addAttribute("articles", articles);
 		
@@ -64,8 +72,16 @@ public class UsrArticleController {
 	}
 	
 	@RequestMapping("/usr/article/detail")
-	public String showDetail(Model model, int id) {
-		Article article = articleService.getForPrintArticle(id);
+	public String showDetail(HttpSession httpSession, Model model, int id) {
+		boolean isLogined = false;
+		int loginedMemberId = 0;
+		
+		if ( httpSession.getAttribute("loginedMemberId") != null) {
+			isLogined = true;
+			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
+		}
+		
+		Article article = articleService.getForPrintArticle(loginedMemberId, id);
 		
 		model.addAttribute("article", article);
 		
@@ -87,7 +103,7 @@ public class UsrArticleController {
 			return ResultData.from("F-A", "로그인 후 이용해주세요.");
 		}
 		
-		Article article = articleService.getForPrintArticle(id);
+		Article article = articleService.getForPrintArticle(loginedMemberId, id);
 		
 		if ( article.getMemberId() != loginedMemberId ) {
 			return ResultData.from("F-2", "권한이 없습니다.");
@@ -117,7 +133,7 @@ public class UsrArticleController {
 			return ResultData.from("F-A", "로그인 후 이용해주세요.");
 		}
 		
-		Article article = articleService.getForPrintArticle(id);
+		Article article = articleService.getForPrintArticle(loginedMemberId, id);
 		
 		if ( article == null ) {
 			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
--- src/main/java/com/ksm/exam/demo/service/ArticleService.java
+++ src/main/java/com/ksm/exam/demo/service/ArticleService.java
@@ -17,14 +17,43 @@ public class ArticleService {
 		this.articleRepository = articleRepository;
 	}
 	
-	public Article getForPrintArticle(int id) {
+	public Article getForPrintArticle(int actorId, int id) {
+		Article article = articleRepository.getForPrintArticle(id);
+		
+		upateForPrintData(actorId, article);
+		
 		return articleRepository.getForPrintArticle(id);
 	}
 	
-	public List<Article> getForPrintArticles() {
-		return articleRepository.getForPrintArticles();
+	public List<Article> getForPrintArticles(int actorId) {
+		List<Article> articles = articleRepository.getForPrintArticles();
+		
+		for(Article article : articles) {
+			upateForPrintData(actorId, article);
+		}
+		return articles;
 	}
 	
+	private void upateForPrintData(int actorId, Article article) {
+		if(article == null) {
+			return;
+		}
+		
+		ResultData actorCanDelteRd = actorCanDelete(actorId, article);
+		article.setExtra_actorCanDelte(actorCanDelteRd.isSuccess());
+	}
+
+	public ResultData actorCanDelete(int actorId, Article article) {
+		if(article == null) {
+			return ResultData.from("F-1", "게시물이 존재하지 않습니다.");
+		}
+		
+		if(article.getMemberId() != actorId) {
+			return ResultData.from("F-1", "권한이 없습니다.");
+		}
+		return ResultData.from("S-1", "게시물 삭제가 가능합니다.");
+	}
+
 	public ResultData<Integer> writeArticle(int memberId, String title, String body) {
 		articleRepository.writeArticle(memberId, title, body);
 		int id = articleRepository.getLastInsertId();
@@ -39,7 +68,7 @@ public class ArticleService {
 	public ResultData<Article> modifyArticle(int id, String title, String body) {
 		articleRepository.modifyArticle(id, title, body);
 		
-		Article article = getForPrintArticle(id);
+		Article article = getForPrintArticle(0, id);
 		
 		return ResultData.from("S-1", Ut.f("%d번 게시물이 수정되었습니다.", id), "article", article);
 	}
