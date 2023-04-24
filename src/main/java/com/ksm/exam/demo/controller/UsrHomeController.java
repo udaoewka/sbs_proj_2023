cb58bd3b30f8f54bc5bfc170a562629b51b52092
udaoewka <ksm54239@gmail.com>
2023-04-24 16:18:12 +0900

    BeforeActionInterceptor에서 Rq객체를 좀 더 확실하게생성되도록 유도

--- src/main/java/com/ksm/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/ksm/exam/demo/controller/UsrHomeController.java
@@ -18,48 +18,4 @@ public class UsrHomeController {
 		return "redirect:/usr/home/main";
 	}
 	
-	@RequestMapping("/usr/home/main2")
-	@ResponseBody
-	public String showMain2() {
-		return "반갑습니다.";
-	}
-	
-	@RequestMapping("/usr/home/main3")
-	@ResponseBody
-	public String showMain3() {
-		return "또 만나요.";
-	}
-	
-	private int count;
-	
-	public UsrHomeController(){
-		count = -1;
-	}
-	
-	@RequestMapping("/usr/home/main4")
-	@ResponseBody
-	public int showMain4() {
-		count++;
-		return count;
-	}
-	
-	@RequestMapping("/usr/home/main5")
-	@ResponseBody
-	public String showMain5() {
-		return "count를 초기화 되었습니다.";
-	}
-	
-	@RequestMapping("/usr/home/getCount")
-	@ResponseBody
-	public int getCount() {
-		return count;
-	}
-	
-	@RequestMapping("/usr/home/doSetCount")
-	@ResponseBody
-	public String doSetCount(int count) {
-		this.count = count;
-		return "count의 값이 " + this.count + "으로 초기화.";
-	}
-	
 }
--- src/main/java/com/ksm/exam/demo/interceptor/BeforeActionInterceptor.java
+++ src/main/java/com/ksm/exam/demo/interceptor/BeforeActionInterceptor.java
@@ -5,18 +5,25 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import com.ksm.exam.demo.service.MemberService;
+import com.ksm.exam.demo.vo.Rq;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class BeforeActionInterceptor implements HandlerInterceptor {
+	private Rq rq;
+	
+	public BeforeActionInterceptor(Rq rq) {
+		this.rq = rq;
+	}
+	
 	@Autowired
 	private MemberService memberService;
 	
 	@Override
 	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
-		// 이제는 Rq 객체가 자동으로 만들어지기 때문에 필요 없음.
+		rq.initOnBeforeActionInterceptor();
 		return HandlerInterceptor.super.preHandle(req, resp, handler);
 	}
 }
