ba5cb1700b70444024efcb63df28dd3bea73c068
udaoewka <ksm54239@gmail.com>
2023-05-02 15:56:22 +0900

    로그인 후, 원래 가려던 페이지로 이동시켜주는 기능 구현

--- src/main/java/com/ksm/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/ksm/exam/demo/controller/UsrMemberController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ksm.exam.demo.service.MemberService;
@@ -84,7 +85,7 @@ public class UsrMemberController {
 	
 	@RequestMapping("/usr/member/doLogin")
 	@ResponseBody
-	public String doLogin(String loginId, String loginPw) {
+	public String doLogin(String loginId, String loginPw, @RequestParam(defaultValue = "/") String afterLoginUri) {
 		if ( rq.isLogined() ) {
 			return rq.jsHistoryBack("이미 로그인되었습니다.");
 		}
@@ -109,7 +110,7 @@ public class UsrMemberController {
 		
 		rq.login(member);
 		
-		return rq.jsReplace(Ut.f("%s님 환영합니다.", member.getNickname()), "/");
+		return rq.jsReplace(Ut.f("%s님 환영합니다.", member.getNickname()), afterLoginUri);
 	}
 	
 	@RequestMapping("/usr/member/getMembers")
